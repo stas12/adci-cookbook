c814c832613af7eee6bc2a71b4f92fae0fcf4479
root <root@ip-10-250-62-86.us-west-2.compute.internal>
2013-06-05 09:55:19 +0000

    Php plugins

--- cookbooks/phpapp/metadata.rb
+++ cookbooks/phpapp/metadata.rb
@@ -6,6 +6,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 depends "nginx"
+depends "php"
 depends "php-fpm"
 depends "openssl"
 depends "ohai"
--- cookbooks/phpapp/recipes/default.rb
+++ cookbooks/phpapp/recipes/default.rb
@@ -8,6 +8,11 @@
 #
 
 include_recipe "nginx"
+include_recipe "php"
+include_recipe "php::module_mysql"
+include_recipe "php::module_curl"
+include_recipe "php::module_apc"
+include_recipe "php::module_gd"
 include_recipe "php-fpm"
 include_recipe "mysql::client"
 include_recipe "mysql::server"
